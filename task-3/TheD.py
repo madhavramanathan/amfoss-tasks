c616f1272e2bf68c520f7d9e654b54e6a6b43283
madhavramanathan <madhavramanathanfocus@gmail.com>
2021-05-25 01:31:15 +0530

    Completed The D program

--- task-3/TheD.py
+++ task-3/TheD.py
@@ -1,12 +1,15 @@
-n=int(input())#3
-int a
-if (n%2!=0) and (n>1):#yes
-    for i in range(n):#i=0,i=1,i=2
-        for j in range(n):#j=0,j=1,j=2,j=0,j=1,j=2
-            if i==(n//2) or j==(n//2):#0!=1 and 0!=1 so no,j=1 so...,
-             a[i][j]="D"#D gets printed,D gets printed,d print,d print,
+n=int(input())
+i=0
+c=1;
+if (n%2!=0) and (n>1):
+    while i>-1:
+        for j in range(n):
+            if j>=(n//2-i) and j<=(n//2+i):
+             print("D",end='')
             else:
-             a[i][j]="*"#*gets printed,*gets printed
-    print(a[i][j])
-
-
+             print("*",end='')
+        if(i==n//2):
+            c=-1
+        i+=c;
+        print()
+        
